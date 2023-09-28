4a80198711d3e95614e59583ce0dfd7411ffc90c
Andrey Filippov <androsphilippos@gmail.com>
2023-09-29 03:41:52 +0500

    fix: fixed ImportCommand

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,26 +1,39 @@
+import { getErrorMessage } from '../../shared/helpers/common.js';
+import { createHousingOffer } from '../../shared/helpers/createHousingOffer.js';
 import { TSVFileReader } from '../../shared/libs/file-reader/tsv-file-reader.js';
 import { ICommand } from './command.interface.js';
-import { CommandName } from './constants.js';
+import { CommandName } from '../../shared/types/command.enum.js';
 
 export class ImportCommand implements ICommand {
   public getName(): string {
     return `--${CommandName.IMPORT}`;
   }
 
+  private onImportedLine(line: string) {
+    const offer = createHousingOffer(line);
+    console.info(offer);
+  }
+
+  private onCompleteImport(count: number) {
+    console.info(`${count} rows imported.`);
+  }
+
   public execute(...parameters: string[]): void {
     const [filename = ''] = parameters;
     const fileReader = new TSVFileReader(filename.trim());
 
+    fileReader.on('line', this.onImportedLine);
+    fileReader.on('end', this.onCompleteImport);
+
     try {
       fileReader.read();
-      console.log(fileReader.toArray());
-    } catch (err) {
-      if (!(err instanceof Error)) {
-        throw err;
+    } catch (error) {
+      if (!(error instanceof Error)) {
+        throw error;
       }
 
       console.error(`Can't import data from file: ${filename}`);
-      console.error(`Details: ${err.message}`);
+      console.error(getErrorMessage(error));
     }
   }
 }
