e3a6fc2865f3a8fd59ea9d8cffa59cac6403c652
Andrey Filippov <androsphilippos@gmail.com>
2023-09-29 03:40:41 +0500

    feat: added typing

--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -1,2 +1,3 @@
+export * from './constants.js';
 export * from './common.js';
 export * from './createHousingOffer.js';
---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,12 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  cities: string[];
+  previewImages: string[];
+  generalImages: string[];
+  housingTypes: string[];
+  housingFeatures: string[];
+  userNames: string[];
+  emails: string[];
+  avatars: string[];
+};
