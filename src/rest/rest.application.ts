417c024ae982db15e32b88c338ecedc4649a22cd
Andrey Filippov <androsphilippos@gmail.com>
2023-10-01 17:41:54 +0500

    feat: added RestConfig and RestSchema

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,11 +1,10 @@
 import { ILogger } from '../shared/libs/logger/index.js';
-
+import { IConfig, RestSchema } from '../shared/libs/config/index.js';
 export class RestApplication {
-  constructor(
-    private readonly logger: ILogger
-  ) {}
+  constructor(private readonly logger: ILogger, private readonly config: IConfig<RestSchema>) {}
 
   public async init() {
     this.logger.info('Application initialization');
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
---
+++ src/shared/libs/config/rest.config.ts
@@ -0,0 +1,26 @@
+import { config } from 'dotenv';
+import { IConfig } from './config.interface.js';
+import { ILogger } from '../logger/index.js';
+import { configRestSchema, RestSchema } from './rest.schema.js';
+
+export class RestConfig implements IConfig<RestSchema> {
+  private readonly config: RestSchema;
+
+  constructor(private readonly logger: ILogger) {
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Can not read .env file. Perhaps the file does not exists.');
+    }
+
+    configRestSchema.load({});
+    configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
+
+    this.config = configRestSchema.getProperties();
+    this.logger.info('.env file found and successfully parsed!');
+  }
+
+  public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
+    return this.config[key];
+  }
+}
