3af25a97d5cbb8d95c627ccd0185708f17eea510
Andrey Filippov <androsphilippos@gmail.com>
2023-10-04 22:28:38 +0500

    feat: added MongoDatabaseClient

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 import { ILogger, PinoLogger } from './shared/libs/logger/index.js';
-import { RestApplication } from './rest/index.js';
 import { IConfig, RestConfig, RestSchema } from './shared/libs/config/index.js';
+import { IDatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
+import { RestApplication } from './rest/index.js';
 import { Component } from './shared/types/index.js';
 
 async function bootstrap() {
@@ -10,6 +11,10 @@ async function bootstrap() {
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<ILogger>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<IConfig<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
+  container
+    .bind<IDatabaseClient>(Component.DatabaseClient)
+    .to(MongoDatabaseClient)
+    .inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from 'inversify';
 import { ILogger } from '../shared/libs/logger/index.js';
 import { IConfig, RestSchema } from '../shared/libs/config/index.js';
+import { IDatabaseClient } from '../shared/libs/database-client/index.js';
+import { getMongoURI } from '../shared/helpers/index.js';
 import { Component } from '../shared/types/component.enum.js';
 
 @injectable()
@@ -8,10 +10,27 @@ export class RestApplication {
   constructor(
     @inject(Component.Logger) private readonly logger: ILogger,
     @inject(Component.Config) private readonly config: IConfig<RestSchema>,
+    @inject(Component.DatabaseClient) private readonly databaseClient: IDatabaseClient,
   ) {}
+
+  private async _initDb() {
+    const mongoUri = getMongoURI(
+      this.config.get('DB_USER'),
+      this.config.get('DB_PASSWORD'),
+      this.config.get('DB_HOST'),
+      this.config.get('DB_PORT'),
+      this.config.get('DB_NAME'),
+    );
+
+    return this.databaseClient.connect(mongoUri);
+  }
 
   public async init() {
     this.logger.info('Application initialization');
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+
+    this.logger.info('Init database…');
+    await this._initDb();
+    this.logger.info('Init database completed');
   }
 }
