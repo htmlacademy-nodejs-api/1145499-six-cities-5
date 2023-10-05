2b54b8b53f1344b09374b0d2860b00b5e832c496
Andrey Filippov <androsphilippos@gmail.com>
2023-10-05 18:28:18 +0500

    fix: separate containers

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,22 +1,14 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
-import { ILogger, PinoLogger } from './shared/libs/logger/index.js';
-import { IConfig, RestConfig, RestSchema } from './shared/libs/config/index.js';
-import { IDatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
 import { RestApplication } from './rest/index.js';
 import { Component } from './shared/types/index.js';
+import { createRestApplicationContainer } from './rest/rest.container.js';
+import { createUserContainer } from './shared/modules/user/user.container.js';
 
 async function bootstrap() {
-  const container = new Container();
-  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
-  container.bind<ILogger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  container.bind<IConfig<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
-  container
-    .bind<IDatabaseClient>(Component.DatabaseClient)
-    .to(MongoDatabaseClient)
-    .inSingletonScope();
+  const appContainer = Container.merge(createRestApplicationContainer(), createUserContainer());
 
-  const application = container.get<RestApplication>(Component.RestApplication);
+  const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
 }
 
