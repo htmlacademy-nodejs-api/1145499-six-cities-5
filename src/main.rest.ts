fba8b9b84424dc666d1f780a186993a29c00850b
Andrey Filippov <androsphilippos@gmail.com>
2023-10-07 13:31:15 +0500

    feat: added createHousingOfferContainer

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -4,9 +4,14 @@ import { RestApplication } from './rest/index.js';
 import { Component } from './shared/types/index.js';
 import { createRestApplicationContainer } from './rest/rest.container.js';
 import { createUserContainer } from './shared/modules/user/user.container.js';
+import { createHousingOfferContainer } from './shared/modules/housing-offer/housing-offer.container.js';
 
 async function bootstrap() {
-  const appContainer = Container.merge(createRestApplicationContainer(), createUserContainer());
+  const appContainer = Container.merge(
+    createRestApplicationContainer(),
+    createUserContainer(),
+    createHousingOfferContainer(),
+  );
 
   const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
