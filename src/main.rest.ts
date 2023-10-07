cc5e7df5565e60ff7244d0f2e56433f793f83c30
Andrey Filippov <androsphilippos@gmail.com>
2023-10-07 22:07:20 +0500

    feat: added CommentContainer + refact other containers

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -3,14 +3,16 @@ import { Container } from 'inversify';
 import { Component } from './shared/types/index.js';
 import { RestApplication } from './rest/index.js';
 import { createRestApplicationContainer } from './rest/rest.container.js';
-import { createUserContainer } from './shared/modules/user/user.container.js';
-import { createHousingOfferContainer } from './shared/modules/housing-offer/housing-offer.container.js';
+import { createUserContainer } from './shared/modules/user/index.js';
+import { createHousingOfferContainer } from './shared/modules/housing-offer/index.js';
+import { createCommentContainer } from './shared/modules/comment/index.js';
 
 async function bootstrap() {
   const appContainer = Container.merge(
     createRestApplicationContainer(),
     createUserContainer(),
     createHousingOfferContainer(),
+    createCommentContainer(),
   );
 
   const application = appContainer.get<RestApplication>(Component.RestApplication);
--- src/shared/modules/user/user.container.ts
+++ src/shared/modules/user/user.container.ts
@@ -6,9 +6,9 @@ import { DefaultUserService } from './default-user.service.js';
 import { UserEntity, UserModel } from './user.entity.js';
 
 export function createUserContainer() {
-  const userContainer = new Container();
-  userContainer.bind<IUserService>(Component.UserService).to(DefaultUserService).inSingletonScope();
-  userContainer.bind<types.ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
+  const container = new Container();
+  container.bind<IUserService>(Component.UserService).to(DefaultUserService).inSingletonScope();
+  container.bind<types.ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
 
-  return userContainer;
+  return container;
 }
