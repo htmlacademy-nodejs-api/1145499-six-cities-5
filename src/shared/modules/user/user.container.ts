7cbfd10a519cecdb3d291a1c1fa20ef53114fed3
Andrey Filippov <androsphilippos@gmail.com>
2023-10-21 20:58:59 +0500

    feat: added UserController

--- src/shared/modules/user/user.container.ts
+++ src/shared/modules/user/user.container.ts
@@ -4,11 +4,14 @@ import { Component } from '../../types/index.js';
 import { IUserService } from './user-service.interface.js';
 import { DefaultUserService } from './default-user.service.js';
 import { UserEntity, UserModel } from './user.entity.js';
+import { UserController } from './user.controller.js';
+import { IController } from '../../libs/rest/index.js';
 
 export function createUserContainer() {
   const container = new Container();
-  container.bind<IUserService>(Component.UserService).to(DefaultUserService).inSingletonScope();
   container.bind<types.ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
+  container.bind<IUserService>(Component.UserService).to(DefaultUserService).inSingletonScope();
+  container.bind<IController>(Component.UserController).to(UserController).inSingletonScope();
 
   return container;
 }
