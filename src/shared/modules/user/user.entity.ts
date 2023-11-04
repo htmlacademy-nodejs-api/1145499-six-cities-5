9fc0fad2461db17be0e14b87de93f791f1fe3e8e
Andrey Filippov <androsphilippos@gmail.com>
2023-11-04 15:22:48 +0500

    WIP: temporary commit

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,6 +1,7 @@
 import { defaultClasses, getModelForClass, prop, modelOptions } from '@typegoose/typegoose';
 import { User, UserType } from '../../types/index.js';
 import { createSHA256 } from '../../helpers/index.js';
+import { DEFAULT_AVATAR_FILE_NAME } from './user.constant.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserEntity extends defaultClasses.Base {}
@@ -13,13 +14,13 @@ export interface UserEntity extends defaultClasses.Base {}
 })
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements User {
-  @prop({ minlength: 1, maxlength: 15, required: true })
+  @prop({ type: String, minlength: 1, maxlength: 15, required: true })
   public name;
 
-  @prop({ unique: true, required: true })
+  @prop({ type: String, unique: true, required: true })
   public email;
 
-  @prop({ required: false })
+  @prop({ type: String, required: false, default: DEFAULT_AVATAR_FILE_NAME })
   public avatar;
 
   @prop({
@@ -28,7 +29,7 @@ export class UserEntity extends defaultClasses.TimeStamps implements User {
   })
   public type;
 
-  @prop({ min: 6, max: 12, required: true })
+  @prop({ type: String, required: true })
   public password: string;
 
   constructor(userData: User) {
